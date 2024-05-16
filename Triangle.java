fe602e21f1552fc0a65bfb14f3bda3a94ab6cffa
Hamsalekhaalavala <124759476+Hamsalekhaalavala@users.noreply.github.com>
2024-05-16 21:04:05 +0530

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Triangle
+{
+ public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+double r,area;
+System.out.println("enter the radius");
+r=sc.nextInt();
+area=3.14*r*r;
+System.out.println("area"+area);
+ }
+}
