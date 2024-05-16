5233e468429be7962b5d1aac10db428adee37518
Hamsalekhaalavala <124759476+Hamsalekhaalavala@users.noreply.github.com>
2024-05-16 21:03:17 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Square
+{
+ public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+int s,area;
+System.out.println("enter the radius");
+s=sc.nextInt();
+area=s*s;
+System.out.println("area"+area);
+ }
+}
